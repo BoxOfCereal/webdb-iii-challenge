1d21b3dacc9f1162fcc312dc111e95f344168d34
Noah <noah.stoeckel@gmail.com>
2019-03-19 20:51:33 -0400

    Adds Get Students By Cohort Helper And Routes For Cohorts

--- data/helpers/cohortsModel.js
+++ data/helpers/cohortsModel.js
@@ -1,19 +1,21 @@
-const db = require('../dbConfig.js');
+const db = require("../dbConfig.js");
 
 module.exports = {
-  findAll: id => {
-    return db('cohorts');
-  },
-  findById: id => {
-    return db('cohorts')
-      .where({ id: id })
-  },
-  add: cohort => {
-    return db.insert(cohort)
-      .into('cohorts')
-      .then(ids => ids[0])
-
-  }
-
-
-}
+	findAll: () => {
+		return db("cohorts");
+	},
+	findById: id => {
+		return db("cohorts").where({ id: id });
+	},
+	add: cohort => {
+		return db
+			.insert(cohort)
+			.into("cohorts")
+			.then(ids => ids[0]);
+	},
+	findStudentsByCohort: id => {
+		return db("cohorts")
+			.join("students", "students.cohort_id", "=", "cohorts.id")
+			.where("cohort_id", "=", `${id}`);
+	}
+};
--- routes/cohorts.js
+++ routes/cohorts.js
@@ -1,36 +1,42 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../data/helpers/cohortsModel.js');
+const db = require("../data/helpers/cohortsModel.js");
 
-
-router.get('/', (req, res) => {
-  db.findAll()
-    .then(cohorts => res.status(200)
-      .json(cohorts))
-    .catch(error => res.status(500)
-      .json({ error: "The cohorts could not be retrieved." }));
+router.get("/", (req, res) => {
+	db.findAll()
+		.then(cohorts => res.status(200).json(cohorts))
+		.catch(error =>
+			res.status(500).json({ error: "The cohorts could not be retrieved." })
+		);
 });
 
-router.post('/', (req, res) => {
-  const newCohort = req.body;
-  db.add(newCohort)
-    .then(id => {
-      db.findById(id)
-        .then(cohort => res.status(201)
-        .json(cohort))
-    })
-    .catch(error => res.status(500)
-      .json({ error: "The cohorts could not be created." }));
+router.post("/", (req, res) => {
+	const newCohort = req.body;
+	db.add(newCohort)
+		.then(id => {
+			db.findById(id).then(cohort => res.status(201).json(cohort));
+		})
+		.catch(error =>
+			res.status(500).json({ error: "The cohorts could not be created." })
+		);
 });
 
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
-  db.findById(id)
-    .then(cohort => res.status(200)
-      .json(cohort))
-    .catch(error => res.status(500)
-      .json({ error: "The cohorts could not be retrieved." }));
+router.get("/:id", (req, res) => {
+	const { id } = req.params;
+	db.findById(id)
+		.then(cohort => res.status(200).json(cohort))
+		.catch(error =>
+			res.status(500).json({ error: "The cohorts could not be retrieved." })
+		);
 });
 
+router.get("/:id/students", (req, res) => {
+	const { id } = req.params;
+	db.findStudentsByCohort(id)
+		.then(students => res.status(200).json(students))
+		.catch(error =>
+			res.status(500).json({ error: "The cohorts could not be retrieved." })
+		);
+});
 
 module.exports = router;
