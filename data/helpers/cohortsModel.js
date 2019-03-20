b93ac83b69e22bc58b8b66e922b7ba34b9729317
Noah <noah.stoeckel@gmail.com>
2019-03-19 20:18:19 -0400

    Adds Get Helper And Routes For A Single Cohort

--- data/helpers/cohortsModel.js
+++ data/helpers/cohortsModel.js
@@ -3,5 +3,12 @@ const db = require('../dbConfig.js');
 module.exports = {
   findAll: id => {
     return db('cohorts');
-  }
+  },
+  findById: id => {
+    return db('cohorts')
+      .where({ id: id })
+  },
+
+
+
 }
--- routes/cohorts.js
+++ routes/cohorts.js
@@ -4,12 +4,20 @@ const db = require('../data/helpers/cohortsModel.js');
 
 
 router.get('/', (req, res) => {
-  const { id } = req.params;
-  db.findAll(id)
+  db.findAll()
     .then(cohorts => res.status(200)
       .json(cohorts))
     .catch(error => res.status(500)
-      .json({ error: "The cohorts could not be retrieved." }))
+      .json({ error: "The cohorts could not be retrieved." }));
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  db.findById(id)
+    .then(cohort => res.status(200)
+      .json(cohort))
+    .catch(error => res.status(500)
+      .json({ error: "The cohorts could not be retrieved." }));
 });
 
 
